c5dc39881dcf13c025765ce7f9bfef50ec3c4d28
JLuisNG <127897120+JLuisNG@users.noreply.github.com>
2023-10-11 23:28:21 -0400

    commit - 13,8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,22 +4,7 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const Storage = 'videoplayer-current-time';
-/*
-player.on('eventName', function (data) {
-   data is an object containing properties specific to that event
-});
-*/
-// _.throttle(func, [wait=0], [options={}])
 
-/*timeupdate
-Triggered as the of the video updates. It generally fires every 250ms, but it may vary depending on the browser.currentTime
-
-{ es un objeto y se accede mediante el argumento data?
-    duration: 61.857
-    percent: 0.049
-    -- USar => seconds: 3.034
-}
-*/
 player.on(
   'timeupdate',
   throttle(function (data) {
